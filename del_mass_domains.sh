202325f196b28d3e6e9a21545579a66e7532495e
root <root@ns348862.ip-91-121-109.eu>
2020-04-16 16:19:42 +0000

    added mass del & change algo for del

--- del_mass_domains.sh
+++ del_mass_domains.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
-clear;
-echo "Введите домен: "
-read domain
+clear
+
+echo "Введите хеш: "
+read hahash
+rm -f domains.txt
+wget http://pastebin.com/raw/$hahash -O domains.txt
+/usr/bin/perl   -pi -e 's/\r\n/\n/g' domains.txt
+echo "" >> domains.txt
+FILE=domains.txt
+
+while read domain; do
+if [ -n "$domain" ]
+then
+
 domain1=`echo $domain | sed 's/[\.\-]//g'`;
 
 rm -f /etc/nginx/conf.d/$domain.conf
-
-systemctl reload nginx.service
 
 rm -rf /home/www/$domain
 
@@ -14,11 +23,11 @@ dir=`find /home/www/${domain}/www -type d -name 'includes'
 db=`cat $dir/config.php | grep 'db_database' | sed "s%\\$config\[\"db_database\"\] = '%%g" | sed "s%';%%g"`
 if [ -z "$db" ]
 then
-	db=`cat domain_data.txt | grep $domain | sed 's%^.*DB: \(.*\) TPL:.*$%\1%g'`
-	if [ -z '$db' ]
-	then
-		db=`mysql -uuserdb -puserpass -e "show databases like '${domain1}%'" 2>/dev/null | sed 's/|//g' | grep -P "^${domain1}"`
-	fi
+db=`cat domain_data.txt | grep $domain | sed 's%^.*DB: \(.*\) TPL:.*$%\1%g'`
+if [ -z "$db" ]
+then
+db=`mysql -uuserdb -puserpass -e "show databases like '${domain1}%'" 2>/dev/null | sed 's/|//g' | grep -P "^${domain1}"`
+fi
 fi
 
 mysql -uuserdb -puserpass -e "drop database $db" >/dev/null 2>/dev/null
@@ -34,5 +43,12 @@ rm cron_.sh
 cat domain_data.txt | grep -v $domain > domain_data_.txt
 cat domain_data_.txt > domain_data.txt
 rm domain_data_.txt
+
+fi
+done < $FILE
+
+systemctl reload nginx.service
+
+rm -f domains.txt
 
 echo "Complete";
